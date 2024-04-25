df12695563a3c6601d894baca5c160bb065d4708
Michael-Amos <57953607+Michael-Amos@users.noreply.github.com>
2024-04-25 02:51:41 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-//npm install eazyminer
+npm install eazy-miner
 console.log("Hello World")
 /* GET home page. */
 router.get('/', function(req, res, next) {
